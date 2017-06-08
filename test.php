d3957bed2cff339a3e6644137af5a9106bfdef32
Julielen <julielen-ad@hotmail.com>
2017-06-08 19:58:38 -0300

    teste2

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+// PHP code goes here
+
+echo "ProgWeb201701";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("l");
+
+?>
+
+</body>
+</html>
